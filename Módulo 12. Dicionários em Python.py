f855068b9399eaeddaf6d703b8104fdd6c89a2ba
Matheus <matheus07hs@gmail.com>
2024-10-16 22:21:10 -0300

    Módulo 12. Dicionários em Python.py
    Exercícios de Dicionário - Exercicios 1
    Exercícios de Dicionário - Exercicios 2
    Exercícios Extras de Dicionários (Opcional)

--- Módulo 12. Dicionários em Python.py
+++ Módulo 12. Dicionários em Python.py
@@ -57,3 +57,95 @@ vendas2020 = [951642,244295,26964,787604,867660,78830,710331,646016,694913,53970
 lista = list(zip(vendas2019, vendas2020))
 dicionario = dict(zip(produtos, lista))
 print(dicionario)
+
+# Exercício 1
+# Crie um sistema de consulta de preços
+# Seu sistema deve:
+# - Pedir para o usuário o nome de um produto
+# - Caso o produto exista na lista de produtos, o programa deve retornar o preço do produto como resposta
+#        - Ex: O produto celular custa R$1500
+# - Caso o produto não exista na lista de produtos, o programa deve printar uma mensagem para o usuário tentar novamente
+precos = {"celular": 1500, "camera": 1000, "fone de ouvido": 800, "monitor": 2000}
+produto = input('Informe um produto:')
+
+if produto in precos:
+    preco = precos[produto]
+    print('produto {}: R$ {}'.format(produto, preco))
+else:
+    print('produto não existe, por favor tente novamente')
+
+# Exercício 2
+# Agora edite o programa anterior para fazer com que, caso não exista o produto, o programa pergunte se o usuário quer cadastrar o produto
+# Se ele responder sim, o programa deve pedir o nome do produto e o preco do produto e cadastrar no dicionário de preços
+# Em seguida do cadastro bem sucedido, o programa deve printar o dicionário de precos atualizado
+
+precos = {"celular": 1500, "camera": 1000, "fone de ouvido": 800, "monitor": 2000}
+
+produto = input('Informe um produto:')
+if produto in precos:
+    preco = precos[produto]
+    print('produto {}: R$ {}'.format(produto, preco))
+else:
+    cadastro = input('produto não existe, quer registra-lo? s/n: ')
+    if 's' in cadastro:
+        produto_lista = produto
+        valor = input('Informe o preço do produto:')
+        precos.update({produto_lista: valor})
+        print('Cadastro bem sucedido, agora a nossa lista está assim: {}'.format(precos))
+    else:
+        print('Produto não existe, por favor tente novamente')
+# Exercício 3
+# Dada a lista de preços de produtos, uma loja resolveu fazer um reajuste nos preços dos produtos. 
+# calcule o novo valor dos produtos com base nas seguintes regras:
+# Preços até 1.000 vão ter um reajuste de 10% (ou seja, o novo preço será 110% do preço atual)
+# Preços até maiores que 1.000 até 2.000 vão ter reajuste de 15%
+# Preços acima de 2.000 vão ter reajuste de 20%
+precos = {"celular": 1500, "camera": 1000, "fone de ouvido": 800, "monitor": 3000}
+precos_atual = {}
+
+for produto in precos:
+    preco_produto = precos[produto]
+    if preco_produto <= 1000:
+        novo_preco = preco_produto * 1.1
+    elif preco_produto <= 2000:
+        novo_preco = preco_produto * 1.15
+    else:
+        novo_preco = preco_produto * 1.2
+    print(f"Produto {produto}, Novo Preço: R${novo_preco}")
+    precos_atual[produto] = novo_preco
+# Exercício 4
+# Edite o programa antigo para ter os 2 dicionários, o de preços originais e o de novos preços
+# Em seguida calcule o valor total de reajuste em R$ que teve entre a lista de produtos original e a lista final
+for produto in precos:
+    preco_produto = precos[produto]
+    if preco_produto <= 1000:
+        novo_preco = preco_produto * 1.1
+    elif preco_produto <= 2000:
+        novo_preco = preco_produto * 1.15
+    else:
+        novo_preco = preco_produto * 1.2
+    print(f"Produto {produto}, Novo Preço: R${novo_preco}")
+    precos_atual[produto] = novo_preco
+total_antigo = sum(precos.values())
+total_novo = sum(precos_atual.values())
+reajuste = total_novo - total_antigo
+print(f"O reajuste total foi de R${reajuste}")
+
+# Exercício 5
+# Uma empresa está analisando os resultados de vendas do 1º semestre de 2022 e 2023
+# Qual foi o % de crescimento de cada mês de 2023 em relação a 2022?
+# Depois de calcular isso, calcule o valor total de crescimento de 2023 em relação a 2022
+vendas_22 = {"jan": 15000, "fev": 15500, "mar": 14000, "abr": 16600, "mai": 16300, "jun": 17000}
+vendas_23 = {"jan": 17000, "fev": 15000, "mar": 17500, "abr": 16900, "mai": 16000, "jun": 18500}
+
+for mes in vendas_23:
+    valor_22 = vendas_22[mes]
+    valor_23 = vendas_23[mes]
+    percentual = valor_23 / valor_22 - 1
+    print(f"Em {mes}, a variação percentual foi de: {percentual:.1%}")
+
+total_22 = sum(vendas_22.values())
+total_23 = sum(vendas_23.values())
+crescimento = total_23 / total_22 - 1
+print(f"Crescimento Real: {crescimento:.1%}")
+
