c641bb926372345d891ff429aaeca502c581ff27
Matheus <matheus07hs@gmail.com>
2024-11-15 00:45:32 -0300

    Módulo 15. List Comprehension.py

--- Módulo 15. List Comprehension.py
+++ Módulo 15. List Comprehension.py
@@ -29,3 +29,16 @@ inadimplentes = []
 inadimplentes = [(cpf, valor_devido) for cpf, valor_devido, qtde_dias in clientes_devedores if qtde_dias > 20]
 
 print(inadimplentes)
+
+# Exercícios
+## 1. Tamanho do Pedido de Compras
+#Nesse exercício vamos avaliar o estoque de uma empresa. Vamos considerar que todos os produtos dessa empresa são comprados em lotes de 500 unidades.
+#- Caso o estoque esteja abaixo de 1000 unidades, devemos fazer um pedido de 500 unidades.#
+#- Caso o estoque esteja abaixo de 200 unidades, devemos fazer um pedido de 1000 unidades.
+#Defina o valor a ser pedido de cada produto para enviar ao time de compras.
+estoque = [('BSA2199',396),('PPF5239',251),('BSA1212',989),('PPF2154',449),('BEB3410',241),('PPF8999',527),('EMB9591',601),('BSA2006',314),('EMB3604',469),('EMB2070',733),('PPF9018',339),('PPF1468',906),('BSA5819',291),('PPF8666',850),('BEB2983',353),('BEB5877',456),('PPF5008',963),('PPF3877',185),('PPF7321',163),('BSA8833',644),('PPF4980',421),('PPF3063',757),('BSA2089',271),('BSA8398',180),('EMB4622',515),('EMB9814',563),('PPF3784',229),('PPF2398',270),('BEB3211',181),('PPF8655',459),('PPF1874',799),('PPF8789',126),('PPF6324',375),('EMB9290',883),('BSA5516',555),('BSA8451',243),('BSA8213',423)]
+
+pedidos = []
+pedidos = [1000 if unidade < 200 else 500 for cod, unidade in estoque]
+
+print(pedidos)
