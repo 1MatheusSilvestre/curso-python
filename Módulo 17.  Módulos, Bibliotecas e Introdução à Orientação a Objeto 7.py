0d3a7142211ee5754db17a8e7678b743c2b48591
Matheus 🧑🏻‍💻 <matheus07hs@gmail.com>
2025-01-10 08:08:32 -0300

    Módulo 17.  Módulos, Bibliotecas e Introdução à Orientação a Objeto 7.py

--- Módulo 17.  Módulos, Bibliotecas e Introdução à Orientação a Objeto 7.py
+++ Módulo 17.  Módulos, Bibliotecas e Introdução à Orientação a Objeto 7.py
@@ -31,3 +31,62 @@ horas, resto_segundos = divmod(resto_segundos, segundos_por_hora)
 minutos, segundos = divmod(resto_segundos, segundos_por_minuto)
 
 print(f'Faltam {dias} dias, {horas} horas, {minutos} minutos e {segundos} segundos para o próximo Ano Novo.') 
+
+## Oferecendo desconto para cliente com base na última compra
+#Suponha que você está trabalhando para uma empresa que deseja rastrear a atividade do cliente. 
+#Uma métrica que eles estão interessados é o tempo que passou desde a última transação do cliente. Se for muito tempo, eles podem oferecer um desconto para o cliente. 
+#Crie um script Python que mostra quanto tempo se passou desde a última compra do cliente. Se faz mais de 30 dias, mostre uma mensagem oferecendo um desconto para o cliente.
+from datetime import datetime
+
+ultima_compra = datetime(2023, 5, 10)
+data_atual = datetime.now()
+diferenca_dias = data_atual - ultima_compra
+
+if diferenca_dias.days > 30:
+    print('Você recebeu um desconto especial! Apareça em nossa loja!')
+
+##Data e hora em diferentes fusos horários
+#Uma empresa tem escritórios em São Paulo, Nova York e Tóquio. Crie um script Python que mostra a data e hora atuais nesses três fusos horários. Exiba, também, se estes escritórios estão abertos ou fechados (9h às 17h).
+from datetime import datetime
+import pytz
+
+escritorios = []
+data_hora_atual = datetime.now(pytz.utc)
+
+data_hora_sp = pytz.timezone('America/Sao_Paulo')
+escritorios.append(('São Paulo', data_hora_sp))
+data_hora_ny = pytz.timezone('America/New_York')
+escritorios.append(('Nova Iorque', data_hora_ny))
+data_hora_toquio = pytz.timezone('Asia/Tokyo')
+escritorios.append(('Tóquio', data_hora_toquio))
+
+for escritorio in escritorios:
+    data_hora_local = data_hora_atual.astimezone(escritorio[1])
+    if 9 <= data_hora_local.hour < 17:
+        print (f'O escritorio de {escritorio[0]} está aberto!')
+    else:
+        print(f'O escritório de {escritorio[0]} está fechado!')
+
+#Calculando a Idade
+#Um usuário fornece sua data de nascimento no formato 'dd/mm/aaaa'. Crie um script em Python que calcula a idade do usuário.
+from datetime import datetime
+
+nascimento_user = input('digite sua data de nascimento (dd/mm/aaaa):  ')
+nascimento_user = datetime.strptime(nascimento_user, '%d/%m/%Y')
+
+data_atual = datatime.now()
+mes_atual = datatime.month
+ano_atual = datatime.year
+idade = data_atual.year - nascimento_user.year
+
+mes_nasc = nascimento_user.month
+dia_nasc = nascimento_user.day
+
+if mes_atual >= mes_nasc:
+    idade -= 1
+    print(idade)
+elif mes_atual == mes_nasc < idade:
+    idade -= 1
+    print(idade)
+
+print(f'Você tem: {idade} anos.')
