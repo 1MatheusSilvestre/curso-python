807f40e729b31fbf87a987a66e25b81fc160f3f4
Matheus 🧑🏻‍💻 <matheus07hs@gmail.com>
2025-01-28 10:59:34 -0300

    Módulo 21. Numpy - a base de Python para Análise de Dados.py

--- Módulo 21. Numpy - a base de Python para Análise de Dados.py
+++ Módulo 21. Numpy - a base de Python para Análise de Dados.py
@@ -1,3 +1,4 @@
+#Exercícios Arrays:
 #Exercício 1
 #Você é um gerente de vendas e tem os dados de vendas de um produto para os últimos 7 dias em uma lista: [127, 90, 201, 150, 210, 220, 115]. Calcule a média de vendas durante a semana.
 import numpy as np
