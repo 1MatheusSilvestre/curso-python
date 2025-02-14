8d284b82014614bcc586770750ec9fba1c52e699
Matheus 🧑🏻‍💻 <matheus07hs@gmail.com>
2025-02-14 10:24:38 -0300

    Módulo 33. Integração Python com APIs e JSON.py

---
+++ Módulo 33. Integração Python com APIs e JSON.py
@@ -0,0 +1,12 @@
+# O que é uma API?
+#É um conjunto de códigos para usar um serviço/site/aplicativo específico. Cada site/ferramenta tem sua própria API. É importante que você saiba ler as APIs que precisar para saber usar
+
+#Um dos padrões mais comuns em API é pegar informações em formato json, uma espécie de dicionário que precisa ser tratada no Python para podermos analisar
+
+#As possibilidades de API são infinitas, vamos fazer 2 exemplos aqui: Cotação de Moedas e Envio de SMS.
+
+#Sites como Google, Youtube, Facebook, Twitter, ArcGis e praticamente qualquer ferramenta/site grande tem uma API
+
+# Quase sempre você precisa de uma conta para consumir uma API. Algumas APIs são abertas, como a https://docs.awesomeapi.com.br/api-de-moedas , mas em muitos casos (como veremos no caso do SMS) vamos precisar ter uma conta ativa para consumir a API\n",
+
+#A Documentação da API (ou exemplos da internet) é a chave para conseguir usar uma API
